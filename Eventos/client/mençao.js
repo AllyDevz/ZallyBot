06490830cd070eb6209d9b564737a77983b1d27a
AllyDevZ <allysong16s@gmail.com>
2022-05-21 19:19:44 -0300

    make it better

--- Eventos/client/mençao.js
+++ Eventos/client/mençao.js
@@ -23,7 +23,7 @@ client.on("messageCreate", message => {
     .setDescription(`> **Opa! Bão?** Me chamo \`${client.user.username}\`, se precisar de ajuda use /help para me adicionar em seu servidor\n __clique aqui para me adicionar__[Invite](https://discord.com/api/oauth2/authorize?client_id=962356709601460234&permissions=1103202674864&scope=applications.commands%20bot)`)
 
    message.reply({embeds: [embed]})
-
+  }
 
 
  
@@ -70,4 +70,4 @@ client.on("messageCreate", message => {
 
    message.reply({embeds: [embed]})
   }
-}});
+});
