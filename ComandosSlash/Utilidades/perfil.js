fcebea4cd4b24bd06c80a854aa29ce9421b7d7a4
AllyDevZ <allysong16s@gmail.com>
2022-05-22 11:12:57 -0300

    make it better

--- ComandosSlash/Utilidades/perfil.js
+++ ComandosSlash/Utilidades/perfil.js
@@ -106,7 +106,7 @@ const avatar = user.avatarURL({ dynamic: true, format: "png", size: 1024 });
    
    const embed = new Discord.MessageEmbed()
    .setTitle(`${client.user.username}`)
-   .setImage(attachment)
+   .setImage({ url: 'attachment://image.png' })
    .setColor(userdb.economia.color)
   await interaction.reply({ embeds: [embed] })  
   } catch(err) {
