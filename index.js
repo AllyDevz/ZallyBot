b5d67e4b708dc4a8ca2b6453c13040418e4de439
AllyDevZ <allysong16s@gmail.com>
2022-11-20 11:19:57 -0300

    make it better

--- index.js
+++ index.js
@@ -69,7 +69,7 @@ const humans = member.guild.members.cache.filter((m) => !m.user.bot).size
             }
 
             if(guildMongoDb.description){
-             welcome.setDescription(guildMongoDb.description)
+             welcome.setDescription(`${guildMongoDb.description}`)
             } else {
              welcome.setDescription(`<@${member.user.id}> Joined the server!\nNow we have **${humans}** members!`)}
 
