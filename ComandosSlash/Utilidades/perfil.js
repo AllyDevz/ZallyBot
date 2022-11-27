f2a2c5fd2ef3b1d5b9eeedbf5b2ccfac52f464dc
blabla <blablablabla@gmail.com>
2022-11-27 14:36:05 -0300

    make it better

--- ComandosSlash/Utilidades/perfil.js
+++ ComandosSlash/Utilidades/perfil.js
@@ -55,7 +55,7 @@ ctx.fillText(`${userdb.economia.sobremim.slice(0,64)}\n${userdb.economia.sobremi
 ctx.font = '23px Uniform';
 ctx.fillStyle = '#F8F8F8';
 ctx.fillText(`: ${abreviar(userdb.economia.rep)}`, 652, 201)
-ctx.fillText(`${abreviar(userdb.economia.money)}`, 717, 229)
+ctx.fillText(`${abreviar(userdb.economia.money)}`, 633, 340)
 ctx.fillText(`${abreviar(userdb.economia.banco)}`, 690, 268)
 ctx.fillText(`${abreviar(userdb.economia.banco + userdb.economia.money)}`, 672, 312)
 
