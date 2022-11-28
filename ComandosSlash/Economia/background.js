e9faf8b71867925a008c8bf5e0c64045395bee42
blabla <blablablabla@gmail.com>
2022-11-28 16:46:09 -0300

    make it better

--- ComandosSlash/Economia/background.js
+++ ComandosSlash/Economia/background.js
@@ -8,13 +8,13 @@ module.exports = {
         {
          name: "background",
          description: "digite o link da sua foto de fundo, Para alterna Para O padrao digite default",
-         type: 3,
+         type: 11,
          required: true
         },
         ],
     run: async (client, interaction) => {
         
-     const sobremim = interaction.options.getString("background")
+     const sobremim = interaction.options.getAttachment('attach')
      
      let userdb = await client.userdb.findOne({
          userID: interaction.user.id
@@ -26,21 +26,8 @@ module.exports = {
          
          userdb = await client.userdb.findOne({ userID: interaction.user.id })
      }
-function filtro(s){
-    if(s === "default"){
-        return "./vFqyhnK.png"
-    } else {
-        return s
-    }
-}
-function e(s){
-    if(s === "default"){
-        return "https://media.discordapp.net/attachments/975219630756986903/975260732990046248/vFqyhnK.png?width=683&height=442"
-    } else {
-        return s
-    }
-}
-userdb.economia.background = filtro(sobremim); userdb.save()
+
+userdb.economia.background = sobremim.url; userdb.save()
      const butao = new Discord.MessageActionRow() 
      interaction.reply({embeds: [new Discord.MessageEmbed()
     .setTitle(`Wallapaper Trocado`)
