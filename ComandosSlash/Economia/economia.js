750319c488ac9260ab2f6c8516ba4eb65bc6b33f
blabla <blablablabla@gmail.com>
2022-11-30 20:47:23 -0300

    make it better

--- ComandosSlash/Economia/economia.js
+++ ComandosSlash/Economia/economia.js
@@ -8,7 +8,7 @@ module.exports = {
         {
             name: 'avatar',
             description: '[👥 Utilidades] Veja o avatar de um usuário',
-            type: "SUBCOMMAND",
+            type: "SUB_COMMAND",
             options: [{
                 name: 'user',
                 description: 'Selecione um usuário, ou envie um ID',
