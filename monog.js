3a3a1baa8dc8ccf21efbd2e120f2e35636abc818
blabla <blablablabla@gmail.com>
2022-11-30 20:19:03 -0300

    make it better

--- monog.js
+++ monog.js
@@ -14,17 +14,21 @@ mongo.connect("mongodb+srv://AllyDevz:all150913a@cluster0.i8sgi.mongodb.net/myFi
 }).catch((err) => {
     console.log('Unable to connect to MongoDB Database.\nError: ' + err)
 })
-app.get('/potato/:id', function (req, res) {
-    console.log(req.params['id']);
-    res.send(req.params['id']);
+app.get('/user/:id', async function (req, res) {
+          let novoserver = await info.findOne({
+                  userID: req.params['id']
+              }) || { economia: { banco: 0, money: 0}, color:"36393e",sobremim: { type: String, default: "Para Altera o Texto va no seu servidor onde esta o bot e dige /sobremim" },rep: { type: Number, default: 0 },usuario: { type: String, default: "Usuario Não Registrado" }}
+    
+    res.json(novoserver.economia.banco);
   });
-app.get('/user/:id', async (req, res) => {
-    const novoserver = new info({
-    userID: req.params['id']
-    })
+app.get('/api/user/:id', async (req, res) => {
+      let novoserver = await info.findOne({
+                  userID: req.params['id']
+              }) || { economia: { banco: 0, money: 0}, color:"36393e",sobremim: { type: String, default: "Para Altera o Texto va no seu servidor onde esta o bot e dige /sobremim" },rep: { type: Number, default: 0 },usuario: { type: String, default: "Usuario Não Registrado" }}
     const q = req.params['id']
-    res.send(`${novoserver}`);
-    console.log(`${novoserver}`)
+    res.json(novoserver);
+    const data = novoserver.userID
+    console.log(data)
     
     });
 var server = app.listen(3000, function() {}); 
